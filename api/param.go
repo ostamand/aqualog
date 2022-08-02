91ebe1add6558a761e87dff345bcb2ba7837b6e0
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2022-08-01 20:07:36 -0400

    Remove unused

--- api/param.go
+++ api/param.go
@@ -74,28 +74,3 @@ func (server *Server) getParams(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, params)
 }
-
-/*
-
-type getValueRequest struct {
-	ID int64 `uri:"id" binding:"required,min=1"`
-}
-
-func (server *Server) getValue(ctx *gin.Context) {
-	var req getValueRequest
-	if err := ctx.BindUri(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-	value, err := server.storage.GetParam(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-	ctx.JSON(http.StatusOK, value)
-}
-*/
