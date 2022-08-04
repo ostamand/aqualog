f5d3e016c9c39acea64f6b213a0f6a116df23662
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2022-08-04 10:14:02 -0400

    Optional migration

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/app/scripts/wait-for.sh postgres:5432
-migrate -path db/migration -database "$DB_SOURCE" -verbose up
+if [ -z "$APPLY_MIGRATION" ]; then
+   /app/scripts/wait-for.sh postgres:5432
+   migrate -path db/migration -database "$DB_SOURCE" -verbose up
+fi
 exec "$1"
