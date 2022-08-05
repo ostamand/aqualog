a29a6a3565dc5587617cf825b22d4957d72b3017
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2022-08-04 20:37:05 -0400

    Typo

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -z "$APPLY_MIGRATION" ]; then
+if [ ! -v "$APPLY_MIGRATION" ]; then
    /app/scripts/wait-for.sh postgres:5432
    migrate -path db/migration -database "$DB_SOURCE" -verbose up
 fi
