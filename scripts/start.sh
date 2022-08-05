2de0c39b9d60fd7729b6737a41809da49d329d5f
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2022-08-04 20:40:45 -0400

    Fix string

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ ! -v "$APPLY_MIGRATION" ]; then
+if [ ! -v $APPLY_MIGRATION ]; then
    /app/scripts/wait-for.sh postgres:5432
-   migrate -path db/migration -database "$DB_SOURCE" -verbose up
+   migrate -path db/migration -database $DB_SOURCE -verbose up
 fi
-exec "$1"
+exec $1
